2575341e672722ec0f58bade06cafb8229321e7c
adorsk <adorsk>
2018-06-08 13:33:27 -0400

    add gradient renderer

--- src/renderers.js
+++ src/renderers.js
@@ -2,6 +2,8 @@ import React from 'react'
 import chroma from 'chroma-js'
 
 import NoiseRenderer from './NoiseRenderer'
+import Prng from './Prng'
+import utils from './utils'
 
 
 let renderers = {}
@@ -64,6 +66,66 @@ renderers['x:h, y:c'] = {
         <rect
           x="0" y="0" height="100%" width="100%"
           fill={fill} />
+      </g>
+    )
+  }
+}
+
+renderers['gradient'] = {
+  renderFn: (opts) => {
+    const { tile } = opts
+    const prng = new Prng()
+    const getColor = () => {
+      const hues = [0, 30, 60]
+      const fuzz = 40
+      const hue = (
+        (
+          utils.sample(hues, 1)[0]
+          + prng.randomInt({min: -fuzz, max: fuzz})
+        ) % 360
+      )
+      return chroma.hsl(hue, 1, .5)
+    }
+    const stops = [
+      // {offset: 0, color: chroma.random().css()},
+      // {offset: 100, color: chroma.random().css()},
+      /*
+      {offset: 0, color: getColor().css()},
+      {offset: 100, color: getColor().css()},
+      */
+      ...(
+        (() => {
+          const numStops = prng.randomInt({min: 2, max: 5})
+          return [...Array(numStops).keys()].map((i) => {
+            return {
+              offset: Math.round((i / numStops) * 100),
+              color: getColor().css(),
+            }
+          })
+        })()
+      ),
+    ]
+    const gradientId = `${tile.key}-gradient`
+    return (
+      <g>
+        <defs>
+          <linearGradient id={gradientId}>
+            {
+              stops.map((stop, i) => {
+                return (
+                  <stop
+                    key={i}
+                    offset={`${stop.offset}%`}
+                    stopColor={stop.color}
+                  />
+                )
+              })
+            }
+          </linearGradient>
+        </defs>
+        <rect
+          x="0" y="0" height="100%" width="100%"
+          fill={`url(#${gradientId})`} />
       </g>
     )
   }
